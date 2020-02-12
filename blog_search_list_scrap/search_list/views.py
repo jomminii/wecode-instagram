a2cbf09064b96dc70ac43d5a9f3c7ee0982c8f75
이종민 <>
2020-02-12 20:12:56 +0900

    blog_search add CSV write and read

--- blog_search_list_scrap/search_list/views.py
+++ blog_search_list_scrap/search_list/views.py
@@ -1,4 +1,4 @@
-#뷰 작성용 임포트
+# 뷰 작성용 임포트
 import json
 
 from .models import SearchList
@@ -10,6 +10,9 @@ from django.http import HttpResponse, JsonResponse
 import requests
 
 from bs4 import BeautifulSoup
+
+# csv 저장용 임포트
+import csv
 
 # 뷰 작성 시작
 class SearchListView(View):
@@ -40,15 +43,34 @@ class SearchListView(View):
             
         )
 
-        for item in zip(post_title, posting_date, blog_url):
-            SearchList(
-                post_title=item[0].text,
-                posting_date=item[1].text,
-                blog_url=item[2].text,
-            ).save()
-        
+
+        with open('./csv/blog_lists.csv', mode='w') as blog_lists:
+            blog_writer = csv.writer(blog_lists)
+            blog_writer.writerow(["post_title", "posting_date","blog_url"])
+
+            for list in zip(post_title, posting_date, blog_url):
+                SearchList(
+                    post_title=list[0].text,
+                    posting_date=list[1].text,
+                    blog_url=list[2].text,
+                ).save()
+                
+                blog_writer.writerow([list[0].text, list[1].text, list[2].text])
+
         return HttpResponse(status=200)
 
     def get(self, request):
-        return JsonResponse({'search list' : list(SearchList.objects.values())}, status=200)
+        result = []
+
+        with open('./csv/blog_lists.csv', mode='r') as blog_lists:
+            reader = csv.reader(blog_lists)
+
+            for list in reader:
+                result.append(list)
+
+        return JsonResponse({'search list' : result}, status=200)
+
+
+
+      # return JsonResponse({'search list' : list(SearchList.objects.values())}, status=200)
 
